583da436c0913bda51f60ad105ec01aeacc13d5b
aa123076-cell <aa123076@gs.gsc.aoyama.ac.jp>
2025-12-31 21:50:18 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const map = new maplibregl.Map({
   container: "map",
-  center: [139.7024, 35.6598], // 中心座標
-  zoom: 16, // ズームレベル
+  center: [138.818740, 37.024360], // 中心座標
+  zoom: 10, // ズームレベル
   style: {
     // スタイル仕様のバージョン番号。8を指定する
     version: 8,
